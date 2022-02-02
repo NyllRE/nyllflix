ff3c25f82d8a074f75237ead8b211342e76280ac
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-02-02 16:28:16 +0300

    template serving

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,8 @@ from .views import *
 app_name='core'
 
 urlpatterns = [
-	path( '', Home.as_view() )
+	path( '', Home.as_view() ),
+	path( 'profile/', Profiles.as_view(), name='profile_list' ),
+	path( 'profile/create/', ProfileCreate.as_view(), name='profile_create' ),
+	path( 'watch/<str:profile_id>/', Watch.as_view(), name='watch' )
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator as methodD
+from .forms import ProfileForm
+from .models import Movie, Profile
 
 
 class Home(View):
    def get( self, request, *args, **kwargs ):
+      if request.user.is_authenticated:
+         return redirect('core:profile_list')
       return render( request, 'index.html' )
+
+@methodD( login_required, name='dispatch' )
+class Profiles(View):
+   def get( self, request, *args, **kwargs ):
+      profiles = request.user.profile.all()
+      return render(request, 'profileList.html',
+       { 'profiles': profiles })
+
+
+@methodD(login_required, name='dispatch')
+class ProfileCreate(View):
+   def get( self, request, *args, **kwargs ):
+      # form for creating profile
+      form = ProfileForm()
+
+      return render( request, 'profileCreate.html', { 'form': form } )
+
+   def post( self, request, *args, **kwargs ):
+      form = ProfileForm(request.POST or None)
+      if form.is_valid():
+         profile = Profile.objects.create(**form.cleaned_data)
+         if profile:
+            request.user.profile.add(profile)
+            return redirect('core:profile_list')
+
+      return render( request, 'profileCreate.html', { 'form': form } )
+
+@methodD(login_required, name='dispatch')
+class Watch(View):
+   def get( self, request, profile_id, *args, **kwargs ):
+      try:
+         profile = Profile.objects.get( uuid=profile_id )
+         movies = Movie.objects.filter( age_limit=profile.age_limit )
+         if profile not in request.user.profile.all():
+            return redirect('core:profile_list')
+         
+         return render( request, 'movieList.html', { 'movies': movies } )
+      except Profile.DoesNotExist:
+            return redirect('core:profile_list')
