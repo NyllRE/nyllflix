5fcfc0f5e55863ea28f06c9fc7333ef68ea4acf6
NyllRE <nyll.redeast@gmail.com>
2023-08-06 17:35:38 +0300

    change staticfiles directory and url

--- django_netflix/settings.py
+++ django_netflix/settings.py
@@ -134,14 +134,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # if in deploy mode
 if not DEBUG:
-    STATIC_ROOT = 'static_root'
+    STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
